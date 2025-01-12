eba4e40ffca45ceac0941a3286fe42d87eb8ced6
ZRMYDYCG <547471919@qq.com>
2025-01-12 22:40:43 +0800

    feat: 支持文章名修改

---
+++ packages/notion-app/src/app/(main)/_components/title.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useMutation } from "convex/react";
+import { Doc } from "../../../../convex/_generated/dataModel";
+import { api } from "../../../../convex/_generated/api";
+import React, { useRef, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface TitleProps {
+  initialData: Doc<"documents">;
+}
+
+const Title = ({ initialData }: TitleProps) => {
+  const update = useMutation(api.documents.update);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(initialData.title || "Untitled");
+
+  const enableInput = () => {
+    setTitle(initialData.title);
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+    }, 0);
+  };
+
+  const disableInput = () => {
+    setIsEditing(false);
+  };
+
+  const onChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    await update({
+      id: initialData._id,
+      title: event.target.value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      disableInput();
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-1">
+      {!!initialData.icon && <p>{initialData.icon}</p>}
+      {isEditing ? (
+        <Input
+          className="h-7 px-2 focus-visible:ring-transparent"
+          ref={inputRef}
+          onClick={enableInput}
+          onBlur={disableInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={title}
+        />
+      ) : (
+        <Button
+          variant="ghost"
+          onClick={enableInput}
+          size="sm"
+          className="font-normal h-auto p-1"
+        >
+          <span className="truncate">{initialData?.title}</span>
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default Title;
