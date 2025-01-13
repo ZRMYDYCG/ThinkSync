75f73dac681e2db0ef1e290a2d2a07dde21f4610
ZRMYDYCG <547471919@qq.com>
2025-01-13 14:24:29 +0800

    feat: 文章封面支持切换及修改

--- packages/notion-app/src/components/cover.tsx
+++ packages/notion-app/src/components/cover.tsx
@@ -2,6 +2,13 @@
 
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/use-cover-image";
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { useParams } from "next/navigation";
 
 interface CoverProps {
   url?: string;
@@ -9,15 +16,47 @@ interface CoverProps {
 }
 
 const Cover = ({ url, preview }: CoverProps) => {
+  const params = useParams();
+  const coverImage = useCoverImage();
+  const removeCoverImage = useMutation(api.documents.removeCoverImage);
+
+  const onRemove = async () => {
+    await removeCoverImage({
+      id: params.documentId as Id<"documents">,
+    });
+  };
+
   return (
     <div
       className={cn(
-        "relative w-full h-[35vh]",
+        "relative w-full h-[35vh] group",
         !url && "h-[12vh]",
         url && "bg-muted",
       )}
     >
       {!!url && <Image src={url} fill className="object-cover" alt="Cover" />}
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={coverImage.onOpen}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Change Cover
+          </Button>
+          <Button
+            onClick={onRemove}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <X className="h-4 w-4 mr-2" />
+            Remove Cover
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
