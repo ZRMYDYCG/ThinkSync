4de1b35004dcece942eb8e350a12a0ae333ba688
ZRMYDYCG <547471919@qq.com>
2025-01-13 15:24:21 +0800

    feat: 展示文章详情时，新增骨架屏

--- packages/notion-app/src/components/cover.tsx
+++ packages/notion-app/src/components/cover.tsx
@@ -10,6 +10,7 @@ import { Id } from "../../convex/_generated/dataModel";
 import { useMutation } from "convex/react";
 import { useParams } from "next/navigation";
 import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface CoverProps {
   url?: string;
@@ -66,6 +67,10 @@ const Cover = ({ url, preview }: CoverProps) => {
       )}
     </div>
   );
+};
+
+Cover.Skeleton = function CoverSkeleton() {
+  return <Skeleton className="w-full h-[12vh] bg-skeleton" />;
 };
 
 export default Cover;
