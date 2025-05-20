2d619ee66a4e0179a88bd1456e708d87985a2096
ZRMYDYCG <547471919@qq.com>
2025-05-20 23:44:27 +0800

    perf: 取消 hover

--- src/app/(main)/_components/user-item.tsx
+++ src/app/(main)/_components/user-item.tsx
@@ -20,7 +20,7 @@ const UserItem = () => {
       <DropdownMenuTrigger>
         <div
           role="button"
-          className="flex items-center text-sm p-3 w-full hover:bg-primary/5"
+          className="flex items-center text-sm p-3 w-full"
         >
           <div className="gap-x-2 flex items-center max-w-[150px]">
             <Avatar className="h-5 w-5">
