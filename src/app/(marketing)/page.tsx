c594d867efc963b293723b2d23ed5b6c283b82d3
resilience <547471919@qq.com>
2025-08-31 23:13:59 +0800

    refactor: remove unused components from MarketingPage

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,10 +1,6 @@
 import Header from './_components/header'
 import Heroes from './_components/heroes'
 import Footer from './_components/footer'
-import Features from './_components/features'
-import Testimonials from './_components/testimonials'
-import Wiki from './_components/wiki'
-import TeamMember from './_components/team-member'
 
 const MarketingPage = () => {
     return (
@@ -12,10 +8,6 @@ const MarketingPage = () => {
             <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 pb-10">
                 <Header></Header>
                 <Heroes></Heroes>
-                <Features></Features>
-                <Wiki></Wiki>
-                <Testimonials></Testimonials>
-                <TeamMember></TeamMember>
             </div>
             <Footer></Footer>
         </div>
